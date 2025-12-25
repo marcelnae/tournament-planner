b10796c51958d596f024dcb8773051e556334604
Marcel Nägle <marceln98@web.de>
2025-12-25 11:00:40 +0100

    WIP: Fix integration tests
    #39

--- tournament-planner-backend/src/main/java/com/marcel/tournament/backend/tournament/TournamentDTO.java
+++ tournament-planner-backend/src/main/java/com/marcel/tournament/backend/tournament/TournamentDTO.java
@@ -1,5 +1,6 @@
 package com.marcel.tournament.backend.tournament;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.ZonedDateTime;
@@ -11,8 +12,12 @@ import java.time.ZonedDateTime;
 public class TournamentDTO {
     private Long id;
     private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
     private ZonedDateTime startDate;
-    private ZonedDateTime endDate; // Optional
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssXXX")
+    private ZonedDateTime endDate; // optional
+
 
     public Tournament toEntity() {
         Tournament tournament = new Tournament();
--- tournament-planner-backend/src/test/resources/test-tournaments-data.sql
+++ tournament-planner-backend/src/test/resources/test-tournaments-data.sql
@@ -1,6 +1,5 @@
 DELETE FROM tournament;
 
-INSERT INTO tournament (name, start_date, end_date) VALUES
-  ('Example Tournament', '2024-06-01T10:00:00+00:00', '2024-06-02T18:00:00+00:00'),
-  ('Spring Cup', '2024-05-10T09:00:00+00:00', '2024-05-12T17:00:00+00:00');
-
+INSERT INTO tournament (id, name, start_date, end_date) VALUES
+  (1001, 'Example Tournament', '2024-06-01T10:00:00+00:00', '2024-06-02T18:00:00+00:00'),
+  (1002, 'Spring Cup', '2024-05-10T09:00:00+00:00', '2024-05-12T17:00:00+00:00');
